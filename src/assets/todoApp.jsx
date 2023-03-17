0bb550120dbb54b77d963337d4888ab13a741c19
Tariq <rafiqtariq19@gmail.com>
2023-03-17 23:48:22 +0530

    localStorage added for as an extra feature

--- src/assets/todoApp.jsx
+++ src/assets/todoApp.jsx
@@ -4,17 +4,24 @@ import List from './listItem'
 
 function todoList() {
 
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState(() => {
+      if(localStorage.getItem('todo')){
+        return localStorage.getItem('todo').split(',');
+      }
+      return [];
+    });
 
     const addList = (inputValue) => {
         setTodoList([...todoList, inputValue]);
-        
+        console.log(todoList, inputValue);
+        localStorage.setItem('todo', [...todoList, inputValue])
     }
 
     const deleteListItem = (index) => {
       let newList = [...todoList];
       newList.splice(index, 1);
       setTodoList([...newList]);
+      localStorage.setItem('todo', newList);
     }
   return (
     <>
