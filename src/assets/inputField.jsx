bf71f251077b879a55ef629c5dd3222515431ce9
Tariq <rafiqtariq19@gmail.com>
2023-03-18 00:04:05 +0530

    Input errors fixed and styling done

--- src/assets/inputField.jsx
+++ src/assets/inputField.jsx
@@ -2,22 +2,35 @@ import React, {useState} from 'react'
 
 function inputField(props) {
   const [inputText, setInputText] = useState(() => '') // first is the state second parameter is a function that changes the state
+
+  const todoExecute = () => {
+    if(inputText.trim() == ""){
+      alert("Enter a Todo")
+      return
+    }
+    props.addListProps(inputText)
+    setInputText("")
+  }
+
   return (
     <div className='inputContainer'>
       <input 
+      autoFocus
       type="text" 
       placeholder='Enter a ToDo'
       value={inputText}
       onChange={e => {
         console.log(e);
         setInputText(e.target.value) //Stores in input text
+      }}
+      onKeyUp={(e) => {
+        if(e.key == "Enter"){
+          todoExecute()
+        }
       }}></input>
       <button onClick={() => {
-        console.log(inputText);
-        props.addListProps(inputText);
-        setInputText("")
+        todoExecute()
       }}>Add ToDo</button>
-      <div>{inputText}</div>
     </div>
   )
 }
